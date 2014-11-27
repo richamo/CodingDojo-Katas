fea2715eff7a77e543f3f673bf89f09c3ede4353
Maxime Sanglan <max_address@yahoo.com>
2014-11-27 11:49:35 +0100

    Labyrinth Kata

---
+++ LABYRINTH/StupidSolver/StupidSolver/StupidSolver.cs
@@ -0,0 +1,48 @@
+﻿using Mazes.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StupidSolver
+{
+    public class StupidSolver : IMazeSolver, IDisposable
+    {
+        private IMaze Maze;
+        private IMouse Mouse;
+
+        public void Init(IMaze maze, IMouse mouse)
+        {
+            Maze = maze;
+            Mouse = mouse;
+        }
+
+        public void YourTurn()
+        {
+            Mouse.TurnLeft();
+            Mouse.Move();
+        }
+
+        public void YouWin()
+        {
+            ReleaseInterfaces();
+        }
+
+        public void YouLoose()
+        {
+            ReleaseInterfaces();
+        }
+
+        private void ReleaseInterfaces()
+        {
+            Maze = null;
+            Mouse = null;
+        }
+
+        public void Dispose()
+        {
+            ReleaseInterfaces();
+        }
+    }
+}
